647102313c92c2a6cbb0ad1b96adff63c0834ab4
Vincent <dddshuo@126.com>
2021-02-27 19:53:36 +0800

    12-5 Basic implementation

--- 12-5 shooting_up_and_down/game_function.py
+++ 12-5 shooting_up_and_down/game_function.py
@@ -1,19 +1,52 @@
 import sys
 import pygame
 
-def check_events(ship):
+from bullet import Bullet
+
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
+    if event.key == pygame.K_UP:
+        # ship.rect.centery -= 1
+        ship.moving_up = True
+    elif event.key == pygame.K_DOWN:
+        # ship.rect.centery += 1
+        ship.moving_down = True
+    elif event.key == pygame.K_SPACE:
+        fire_bullet(ai_settings, screen, ship, bullets)
+
+def check_keyup_events(event, ai_settings, screen, ship, bullets):
+    if event.key == pygame.K_UP:
+        ship.moving_up = False
+    if event.key == pygame.K_DOWN:
+        ship.moving_down = False
+
+def check_events(ai_settings, screen, ship, bullets):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                # ship.rect.centery -= 1
-                ship.moving_up = True
-            if event.key == pygame.K_DOWN:
-                # ship.rect.centery += 1
-                ship.moving_down = True
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP:
-                ship.moving_up = False
-            if event.key == pygame.K_DOWN:
-                ship.moving_down = False
+            check_keyup_events(event, ai_settings, screen, ship, bullets)
+
+def update_bullets(bullets, ai_settings, ship):
+    bullets.update()
+    for bullet in bullets.copy():
+        # print(bullet.rect.centery)
+        # print("ship -->", ship.rect.centery)
+        # print("bullet-->", bullet.rect.centery)
+        if bullet.rect.centerx >= ai_settings.screen_width:
+            bullets.remove(bullet)
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    # 当按下一次空格键时，创建一颗子弹，并将其加入到编组bullets中
+    # print("--start fire--")
+    new_bullet = Bullet(ai_settings, screen, ship)
+    bullets.add(new_bullet)
+
+def update_screen(ai_settings, screen, ship, bullets):
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+
+    pygame.display.flip()
--- 12-5 shooting_up_and_down/settings.py
+++ 12-5 shooting_up_and_down/settings.py
@@ -6,14 +6,12 @@ class Settings():
         self.screen_width = 700
         self.screen_height = 500
         self.bg_color = (135, 206, 235)
-
         # 飞船速度设置
         self.ship_speed_factor = 1.5
-
         # 子弹设置
-        self.bullet_width = 3
-        self.bullet_height = 5
-        self.bullet_color = (230,230,230)
+        self.bullet_width = 15
+        self.bullet_height = 3
+        self.bullet_color = 60,60,60
 
 
 
